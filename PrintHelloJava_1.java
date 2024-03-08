74613ece0b796b6ca9a3aa1d669d1f58e0dbe6ee
Soumen Saha <soumen1310.official@gmail.com>
2024-03-08 12:20:54 +0530

    print a simple value and string

---
+++ PrintHelloJava_1.java
@@ -0,0 +1,18 @@
+// Program to print a simple string and a value of a veriable in java
+
+// To COMPILE -> javac printHelloJava_1.java
+// To RUN -> java printHelloJava  (here we have to put class name) 
+
+class PrintHelloJava_1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Java");
+		
+		int a = 1234567890;
+		System.out.print(a);
+	}
+} 
+
+// System.out.println("abc") -> means it will print the string and also add a next line.
+// System.out.print("abc") -> means it will print only.
